d7350be96cfacce20ce14d9d21429a58cc9d56de
Pilse <gogosky1175@gmail.com>
2021-01-08 23:08:45 +0900

    implemeting command_line
    update_command_line

---
+++ command_line.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <string.h>
+#include "header.h"
+
+bool command_line(void)
+{
+    char *string = NULL;
+    int is_pipe = 0;
+    int is_redirection = 0;
+    char **arg = NULL;
+
+    gets(string);
+    parse(string);
+    arg = token;
+    if (!strcmp(arg[0], "quit") || !strcmp(arg[0], "exit"))
+        break;
+    else
+    {
+        if (!strcmp(token[0], "cd"))
+            change_directory(arg);
+
+        is_pipe = is_pipe(arg);
+        is_redirection = is_redirection(arg);
+        if (is_redirection)
+            redirection(arg);
+        else if (is_pipe)
+            pipe(token);
+        else
+            execvp(*arg, arg);
+    }
+    return false;
+}
