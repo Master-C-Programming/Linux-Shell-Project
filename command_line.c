f4321d1b07f2401186a98241790443bb8d443435
Pilse <gogosky1175@gmail.com>
2021-01-12 23:31:13 +0900

    modified_command_line2

--- command_line.c
+++ command_line.c
@@ -1,36 +1,53 @@
-#include<stdio.h>
-#include<string.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 #include <stdbool.h>
 #include "header.h"
 
 bool command_line()
 {
-    char* command=NULL;
+    char *arg[1024];
     bool is_pipe = false;
     bool is_redirection = false;
+    int type = 0;
+    int index = 0;
 
-    gets(command);
-    parsing(command);
-    char **arg = token;
-
-    if(!strcmp(arg[0],"quit") || !strcmp(arg[0],"exit"))
-        return false;
-    else
+    fgets(input, 1024, stdin);
+    tok = tokens;
+    ptr = input;
+    type = get_token(&arg[index++]);
+    if (type == 2)
     {
-        if(!strcmp(arg[0],"cd"))
+        while (type == 2)
+            type = get_token(&arg[index++]);
+    }
+
+    if (type == 1)
+    {
+        if (!strcmp(arg[0], "quit") || !strcmp(arg[0], "exit"))
+            return false;
+        else
         {
-            change_direcory(arg);
+            if (!strcmp(arg[0], "cd"))
+            {
+                change_directory(arg);
+                return true;
+            }
+            is_pipe = is_pipe(arg);
+            is_redirection = is_redirection(arg);
+            if (is_redirection)
+                redirection(arg);
+            else if (is_pipe)
+                pipe(arg);
+            else
+                execvp(*arg, arg);
             return true;
         }
-        is_pipe = is_pipe(arg);
-        is_redirection = is_redirection(arg);
-        if(is_redirection)
-            redirection(arg);
-        else if(is_pipe)
-            pipe(arg);
-        else
-            execvp(*arg,arg);
+    }
+    // EMPERSAND의 경우 
+    // 아직 구현을 하지 않았습니다.
+    if(type == 0)
+    {
         return true;
     }
 }
