c1d8e584873fedec0e2d3ded88808f5b9ceeaec8
naekang <66901361+naekang@users.noreply.github.com>
2021-01-23 10:55:20 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,11 @@
 #include "header.h"
 
-// int argc : 메인함수에 전달되는 정보의 개수
-// char **argv : 메인함수에 전달되는 실직적인 정보(문자열의 배열)
-int main(int argc, char **argv, char **env)
+int main(int argc, char **argv)
 {
-    char str[1024];
-
     while (1)
     {
-        printf("UOSCS# ");
-        fgets(str, sizeof(str) - 1, stdin);
-        if (command_line(str) == 0)
+        printf("UOSCS:%s# ", current_directory);
+        if (!command_line())
             break;
     }
 
