5ab8ff56b0f452f196675e5fdefeb327dbedef1d
naekang <66901361+naekang@users.noreply.github.com>
2021-01-23 10:54:44 +0900

    Update header.h

--- header.h
+++ header.h
@@ -9,29 +9,25 @@
 #include <stdbool.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <pwd.h>
 
 // 상수 및 매크로 정의부
-#define FALSE 0
-#define TRUE 1
-
 #define AMPERSAND 0
 #define EOL 1
 #define ARG 2
+#define SIZE 1024
 
 // 사용자 정의 type정의부
-typedef struct
-{
-
-}
 
 // 전역 변수 선언
+char current_directory[SIZE] = "~";
+char previous_directory[SIZE];
 char input[SIZE];
 char tokens[SIZE];
 char *ptr;
 char *tok;
 
 // 함수 선언부
-int parse(void);
 int get_token(char **arg);
 void change_directory(char **arg, int index);
 bool is_pipe(char **arg, int size);
